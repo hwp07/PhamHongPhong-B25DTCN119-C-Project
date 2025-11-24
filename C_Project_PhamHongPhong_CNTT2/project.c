a7c8e894294dd28a0736d6d6dff6808e544d2d56
hwp07 <hongphongpham2108@gmail.com>
2025-11-25 00:32:42 +0700

    Add files via upload

--- C_Project_PhamHongPhong_CNTT2/project.c
+++ C_Project_PhamHongPhong_CNTT2/project.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <string.h>
+#include <ctype.h>
 #define MAX 100
 
 struct Product {
@@ -11,11 +12,11 @@ struct Product {
 };
 
 struct Product prd[MAX] = {
-    {"P001", "Gao ST25", "Kg", 100, 1},
-    {"P002", "Duong Cat Trang", "Kg", 50, 1},
-    {"P003", "Sua Tuoi Vinamilk", "Hop", 200, 1},
-    {"P004", "Mi Tom Hao Hao", "Thung", 30, 0},
-    {"P005", "Nuoc Ngot Pepsi", "Chai", 120, 1}
+    {"P001", "Gao ST25", "Kg", 100},
+    {"P002", "Duong Cat Trang", "Kg", 50},
+    {"P003", "Sua Tuoi Vinamilk", "Hop", 200},
+    {"P004", "Mi Tom Hao Hao", "Thung", 30},
+    {"P005", "Nuoc Ngot Pepsi", "Chai", 120}
 };
 
 
@@ -37,11 +38,15 @@ void showHistory();
 
 
 int size = 5;
-struct Product prd[MAX];
+
 
 int main(){
 	int choice;
 	
+	for(int i = 0;i < MAX;i++){
+		prd[i].status = 1;
+	}
+		
 	do {
 		printf("\n+---------------MENU-------------+\n");
 		printf("|1. Them ma hang moi.            |\n");
@@ -100,66 +105,104 @@ int main(){
 }
 
 
-void addProduct(){
-	struct Product newPrd;
-	
-	if(size >= MAX){
-		printf("Mang day!");
-		return;
-	}
-	
-	printf("\nNhap ma san pham: ");
-	fgets(newPrd.productID, sizeof(newPrd.productID), stdin);
-	newPrd.productID[strcspn(newPrd.productID, "\n")] = 0;
-	if(strlen(newPrd.productID) == 0){
-		printf("Khong duoc de trong ma san pham!");
-		return;
-	}
-	
-	for(int i = 0; i < size;i++){
-		if(strcmp(newPrd.productID, prd[i].productID) == 0){
-			printf("Ma san pham bi trung!!!");
-			return;
+void addProduct() {
+    if (size == MAX) {
+        printf("Danh sach day! Them moi hang hoa that bai!\n");
+        return;
+    }
+
+    struct Product newPrd;
+
+    while (1) {
+        printf("\nNhap ma san pham: ");
+        fgets(newPrd.productID, sizeof(newPrd.productID), stdin);
+        newPrd.productID[strcspn(newPrd.productID, "\n")] = 0;
+
+        if (strlen(newPrd.productID) == 0) {
+            printf("Khong duoc de trong ma san pham!\n");
+            continue;
+        }
+
+        int flag = 0;
+        for (int i = 0; i < size; i++) {
+            if (strcmp(newPrd.productID, prd[i].productID) == 0) {
+                printf("Ma san pham bi trung! Nhap lai.\n");
+                flag = 1;
+                break;
+            }
+        }
+
+        if (!flag) {
+        	break;
 		}
-	}
-	
-	
-	printf("Nhap ten san pham: ");
-	fgets(newPrd.name, sizeof(newPrd.name), stdin);
-	newPrd.name[strcspn(newPrd.name, "\n")] = 0;
-    if(strlen(newPrd.name) == 0){
-	    printf("Khong duoc de trong ten san pham!\n");
-        return;
     }
-	
-	
-	printf("Nhap don vi (Cai/Kg/Hop): ");
-	fgets(newPrd.unit, sizeof(newPrd.unit), stdin);
-	newPrd.unit[strcspn(newPrd.unit, "\n")] = 0;
-	if(strlen(newPrd.unit) == 0){
-        printf("Khong duoc de trong don vi!\n");
-        return;
-    }
+
+    do {
+        printf("Nhap ten san pham: ");
+        fgets(newPrd.name, sizeof(newPrd.name), stdin);
+        newPrd.name[strcspn(newPrd.name, "\n")] = 0;
+
+        if (strlen(newPrd.name) == 0) {
+            printf("Khong duoc de trong ten san pham!\n");
+        }
+    } while (strlen(newPrd.name) == 0);
     
-	
-	printf("Nhap so luong: ");
-	scanf("%d", &newPrd.qty);
-	getchar();
-	if(newPrd.qty <= 0){
-        printf("So luong phai > 0!\n");
-        return;
-    }
     
+	do {
+	    printf("Nhap don vi (Cai/Kg/Hop): ");
+	    fgets(newPrd.unit, sizeof(newPrd.unit), stdin);
+	    newPrd.unit[strcspn(newPrd.unit, "\n")] = 0;
 	
-	printf("Nhap trang thai (1-Con hang, 0-Het hang): ");
-	scanf("%d", &newPrd.status);
-	getchar();
+	    if (strlen(newPrd.unit) == 0) {
+	        printf("Khong duoc de trong don vi!\n");
+	        continue;
+	    }
 	
-	prd[size] = newPrd;
-	size++;
+	    int hasDigit = 0;
+	    for (int i = 0; i < sizeof(newPrd.unit); i++) {
+	        if (newPrd.unit[i] == '\0') {
+	        	break; 
+			}
+			
+	        if (isdigit(newPrd.unit[i])) {
+	            hasDigit = 1;
+	            break;
+	        }
+	    }
 	
-	printf("THEM MOI THANH CONG!\n\n");
+	    if (hasDigit) {
+	        printf("Don vi khong duoc chua so! Nhap lai.\n");
+	        continue;
+	    }
+	
+	    break;
+	} while (1);
+
+
+    do {
+        printf("Nhap so luong: ");
+        if (scanf("%d", &newPrd.qty) != 1) {
+            printf("Vui long nhap so!\n");
+            while(getchar() != '\n');
+            continue;
+        }
+        getchar();
+
+        if (newPrd.qty <= 0) {
+            printf("So luong phai > 0!\n");
+            continue;
+        }
+
+        break;
+    } while (1);
+
+    newPrd.status = 1; 
+    prd[size] = newPrd;
+    size++;
+
+    printf("\nTHEM MOI THANH CONG!\n");
 }
+
 
 
 void updateProduct(){
@@ -177,17 +220,18 @@ void updateProduct(){
     }
 
     if(index == -1){
-        printf("Cap nhat that bai! Khong tim thay ma san pham.\n");
+    	printf("Khong tim thay hang hoa!\n");
+        printf("Them moi hang hoa that bai!\n");
         return;
     }
 
-    printf("\n--- THONG TIN HIEN TAI ---\n");
+    printf("\n    THONG TIN HIEN TAI    \n");
     printf("ID: %s\n", prd[index].productID);
     printf("Ten: %s\n", prd[index].name);
     printf("Don vi: %s\n", prd[index].unit);
     printf("So luong: %d\n", prd[index].qty);
 
-    printf("\n--- NHAP THONG TIN MOI ---\n");
+    printf("\n    NHAP THONG TIN MOI    \n");
 
     char newName[50];
     printf("Nhap ten moi (de trong neu giu nguyen): ");
@@ -195,35 +239,62 @@ void updateProduct(){
     newName[strcspn(newName, "\n")] = 0;
 
     if(strlen(newName) == 0){
-    	strcpy(newName, prd[index].name); //
+    	strcpy(newName, prd[index].name);
     }else if (strlen(newName) > 0){
         strcpy(prd[index].name, newName);
     }
 
-    char newUnit[10];
-    printf("Nhap don vi moi (de trong neu giu nguyen): ");
-    fgets(newUnit, sizeof(newUnit), stdin);
-    newUnit[strcspn(newUnit, "\n")] = 0;
+	char newUnit[10];
+	while (1) {
+	    printf("Nhap don vi moi (de trong neu giu nguyen): ");
+	    fgets(newUnit, sizeof(newUnit), stdin);
+	    newUnit[strcspn(newUnit, "\n")] = 0;
+	
+	    if (strlen(newUnit) == 0) {
+	        break;
+	    }
+	
+	    int hasDigit = 0;
+	    for (int i = 0; i < sizeof(newUnit); i++) {
+	        if (newUnit[i] == '\0') break;
+	        if (isdigit(newUnit[i])) {
+	            hasDigit = 1;
+	            break;
+	        }
+	    }
+	
+	    if (hasDigit) {
+	        printf("Don vi khong duoc chua so!\n");
+	        continue;
+	    }
+	
+	    strcpy(prd[index].unit, newUnit);
+	    break;
+	}
 
-    if(strlen(newUnit) == 0){
-    	
-    }else if (strlen(newUnit) > 0){
-        strcpy(prd[index].unit, newUnit);
-    }
 
-    int newQty;
-    printf("Nhap so luong moi (-1 de giu nguyen): ");
-    scanf("%d", &newQty);
-    getchar();
+	int newQty;
+	while (1) {
+	    printf("Nhap so luong moi (-1 de giu nguyen): ");
+	    if (scanf("%d", &newQty) != 1) {
+	        printf("Vui long nhap so!\n");
+	        while (getchar() != '\n');
+	        continue;
+	    }
+	    getchar();
+	
+	    if (newQty == -1) {
+	        break;
+	    } else if (newQty >= 0) {
+	        prd[index].qty = newQty;
+	        break;
+	    } else {
+	        printf("So luong khong hop le!\n");
+	    }
+	}
 
-    if(newQty >= 0){
-        prd[index].qty = newQty;
-    }else if (newQty < -1){
-        printf("Cap nhat that bai! So luong phai >= 0.\n");
-        return;
-    }
 
-    printf("\nCap nhat thong tin thanh cong!\n");
+    printf("\nThem moi hang hoa thanh cong!\n");
 }
 
 
